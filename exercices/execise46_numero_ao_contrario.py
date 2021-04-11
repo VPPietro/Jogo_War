b28f99f561b615943fd771215fbafdcb6034558f
Pietro Paraventi Vanelli <pietroparaventivanelli@gmail.com>
2021-04-11 20:11:53 -0300

    remove_unwanted

--- exercices/execise46_numero_ao_contrario.py
+++
@@ -1,2 +0,0 @@
-numero = str(input('digite um numero de tres digitos '))
-print(f'o numero digitado ao contrario (pq sim) é: {numero[::-1]}')
--- exercices/execise47_cada_digito_uma_linha.py
+++
@@ -1,2 +0,0 @@
-numero = str(input('digite um numero de 4 digitos: '))
-print(f'o numero é: \n{numero[::][0]} \n{numero[::][1]} \n{numero[::][2]} \n{numero[::][3]}')
