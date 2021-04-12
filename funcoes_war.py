920d5c2feac781dee343c6b5563e3c63763966fd
Pietro Paraventi Vanelli <pietroparaventivanelli@gmail.com>
2021-04-12 19:22:47 -0300

    update

--- funcoes_war.py
+++ funcoes_war.py
@@ -2,15 +2,11 @@ from random import randint
 
 
 pais_fronteira = {'Brasil': 'Argentina Colombia Egito', 'Argentina': 'Brasil Colombia',
-                  'Colombia': 'Brasil Mexico', 'EUA': 'Mexico Russia UK', 'UK': 'EUA França Alemanha',
-                  'França': 'Alemanha UK Egito', 'Alemanha': 'França UK Egito Russia',
-                  'Egito': 'França Alemanha Brasil Africa do Sul', 'Africa do Sul': 'Egito',
-                  'Russia': 'Alemanha China EUA', 'China': 'Russia'
+                  'Colombia': 'Brasil Mexico', 'Eua': 'Mexico Russia Uk', 'Uk': 'Eua França Alemanha',
+                  'França': 'Alemanha Uk Egito', 'Alemanha': 'França Uk Egito Russia',
+                  'Egito': 'França Alemanha Brasil Africa Do Sul', 'Africa Do Sul': 'Egito',
+                  'Russia': 'Alemanha China Eua', 'China': 'Russia', 'Mexico': 'Colombia Eua'
                   }
-jogador_pais_exercito = {'Colombia': 3, 'Mexico': 3, 'EUA': 3, 'China': 3, 'Argentina': 3, 'Alemanha': 3
-                         }
-computador_pais_exercito = {'Brasil': 3, 'UK': 3, 'França': 3, 'Egito': 3, 'Africa do Sul': 3, 'Russia': 3
-                            }
 
 
 def distribui_6_exercitos(pais_exercito):
@@ -97,24 +93,13 @@ def joga_jogador(jogador_pais_exercito, computador_pais_exercito, fronteira=pais
         dados_computador = joga_dado_defesa(computador_pais_exercito[destino])
         for x in range(len(dados_jogador)):
             if max(dados_jogador) > max(dados_computador):
-                    print(x)
                     computador_pais_exercito[destino] -= 1
             else:
-                print(x)
                 jogador_pais_exercito[atacante] -= 1
             dados_jogador.pop()
             dados_computador.pop()
-        print(jogador_pais_exercito)
-        print(computador_pais_exercito)
-
     else:
         print('\n' * 10)
         print(f'O País de destino ({destino}) não faz fronteira com o País que esta atacando ({atacante})\n')
         return joga_jogador(jogador_pais_exercito, computador_pais_exercito, adiciona_exercito=False)
-
-
-
-
-
-
-print(joga_jogador(jogador_pais_exercito, computador_pais_exercito, pais_fronteira))
+    return jogador_pais_exercito, computador_pais_exercito
