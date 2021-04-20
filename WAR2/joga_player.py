90c3a881c55582b7e368cd5407506eeb979104e3
Pietro Paraventi Vanelli <pietroparaventivanelli@gmail.com>
2021-04-19 21:24:28 -0300

    update

--- WAR2/joga_player.py
+++ WAR2/joga_player.py
@@ -1,2 +1,21 @@
-class A:
-    pass
+from random import randint
+
+
+class Jogada:
+
+    def __init__(self, jpe, ppe, add_exercito=False):
+        self.__player_p_e = jpe
+        self.__pc_p_e = ppe
+        self.__add_exercito = add_exercito
+
+    def get_player(self):
+        return self.__player_p_e
+
+    def distribui_novos_exercitos(self):
+        for x in range(6):  # Quantidade a adicionar de exército dentro do range
+            self.__player_p_e[ list(self.__player_p_e.keys())[randint(0, len(self.__player_p_e) - 1)] ] += 1
+        return self.__player_p_e
+
+    def joga_jogador(self, origem, destino):
+        pass
+
--- WAR2/main.py
+++ WAR2/main.py
@@ -1,5 +1,20 @@
 from define_jogador import Jogador
+from joga_player import Jogada
+from random import randint
 
 p1 = Jogador(input('Digite seu nome: '))
 player_p_e = Jogador.get_jogador(p1)
 pc_p_e = Jogador.get_computador(p1)
+
+# teste
+jogada_player = Jogada(player_p_e, pc_p_e)
+print(player_p_e)
+print(pc_p_e)
+
+# teste
+origem = player_p_e[list(player_p_e.keys())[randint(0, len(player_p_e) - 1)]]
+destino = pc_p_e[list(pc_p_e.keys())[randint(0, len(pc_p_e) - 1)]]
+Jogada.joga_jogador(jogada_player, origem, destino)
+print(origem, destino)
+
+
